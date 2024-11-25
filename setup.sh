d5a29bc0fe1ba4417a1f7d5d5fd8cdbc21bf5e1a
Tumi Oguntola <tumi.oguntola@gmail.com>
2024-11-25 18:28:40 -0500

    Removed storage of SQL container

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ if [ ! -d "$TARGET_DIR" ]; then
     mkdir -p "$TARGET_DIR"
 fi
 
-docker run --name=$CONTAINER_NAME -d --mount type=bind,source="$(pwd)"/$TARGET_DIR,target=/var/lib/mysql mysql/mysql-server:$CONTAINER_TAG 
+docker run --name=$CONTAINER_NAME -d -p 3306:3306 mysql/mysql-server:$CONTAINER_TAG 
+# docker run --name=$CONTAINER_NAME -d -p 3306:3306 --mount type=bind,source="$(pwd)"/$TARGET_DIR,target=/var/lib/mysql mysql/mysql-server:$CONTAINER_TAG 
 until [ "$( docker container inspect -f '{{.State.Health.Status}}' $CONTAINER_NAME )" = "healthy" ]; do
     sleep 0.1;
 done;
@@ -18,15 +19,19 @@ TEMP_PASSWORD=$(docker logs $CONTAINER_NAME 2>&1 | grep GENERATED | awk '{print 
 # echo "PASSWORD: $TEMP_PASSWORD"
 docker exec $CONTAINER_NAME mysql -u$USER_NAME -p$TEMP_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$PASSWORD';" --connect-expired-password
 docker exec $CONTAINER_NAME mysql -u$USER_NAME -p$PASSWORD -e "drop schema test;" --connect-expired-password
+docker exec $CONTAINER_NAME mysql -u$USER_NAME -p$PASSWORD -e "CREATE USER 'root'@'%' IDENTIFIED BY '$PASSWORD';" --connect-expired-password
+docker exec $CONTAINER_NAME mysql -u$USER_NAME -p$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';" --connect-expired-password
+docker exec $CONTAINER_NAME mysql -u$USER_NAME -p$PASSWORD -e "FLUSH PRIVILEGES;" --connect-expired-password
 
 
 echo "Your mysql database has been setup successfully!"
 echo "Container Name: $CONTAINER_NAME"
 echo "User Name: $USER_NAME"
 echo "Password: $PASSWORD"
-echo "Target Directory: $TARGET_DIR"
+# echo "Target Directory: $TARGET_DIR"
 
-echo "The database is accessible at localhost:3306, the password is $PASSWORD, and the data is stored in $(pwd)$TARGET_DIR."
+# echo "The database is accessible at localhost:3306, the password is $PASSWORD, and the data is stored in $(pwd)$TARGET_DIR."
+echo "The database is accessible at localhost:3306, the password is $PASSWORD, and data is lost when the container is stopped."
 echo "To run the databse, use the command 'source db.sh' in this directory."
 echo "To stop the database, use the command 'source stop.sh' in this directory."
 echo "Make sure you run stop.sh before you close docker or shut down your computer."
