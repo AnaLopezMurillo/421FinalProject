ed04019f22ea7aed5fc26c3db6fa9e41cd84e62b
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-11-26 13:39:34 -0500

    Added input clear functionality + cleaned frontend
    changed some variable names to read layouts easier
    add record on successful input now clears input fields

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import PySimpleGUI as sg
-import db
+# import db
 
-cursor = db.get_cursor()
+# cursor = db.get_cursor()
 
 # vars
 username = ''
 password = ''
 
-def login_layout():
+def login():
     return [
         [sg.Text("Login Page", font='Bold', justification='center', expand_x=True)],
         [sg.Text('Username', justification='center', expand_x=True), sg.InputText(key='-USERNAME-')],
@@ -15,31 +15,31 @@ def login_layout():
         [sg.Button("Exit"), sg.Text("", expand_x=True), sg.Button("Login")]
     ]
 
-def home_layout():
+def home():
     return [
         [sg.Button("Back to Login"), sg.Text("", key='-USERNAME-DISPLAY-', font='bold', justification='center', expand_x=True), sg.Button("Profile"),],
         [sg.Text("Posts will go here"),sg.Button("Add Record"),],
         [sg.Button("Exit")]
     ]
 
-def profile_layout():
+def profile():
     return [
         [sg.Button("Home"), sg.Text("Profile", font='bold', justification='center', expand_x=True)],
         [sg.Text("Profile info + scores will go here")],
         [sg.Button("Exit")]
     ]
 
-def add_record_layout():
+def add_record():
     return [
         [sg.Button("Home"), sg.Text("Add Record", font='bold', justification='center', expand_x=True), sg.Button("Profile")],
         [sg.Text("Term", justification='center', expand_x=True), sg.InputText(key='-TERM-')],
         [sg.Text("Definition",justification='center', expand_x=True), sg.InputText(key='-DEF-')],
-        [sg.Text("", key='-ADDRECORD-DISPLAY-')],
-        [sg.Button("Exit"), sg.Text("", expand_x=True), sg.Button("Submit")]
+        [sg.Text("", key='-ADDRECORD-DISPLAY-'), sg.Text("", expand_x=True), sg.Button("Submit")],
+        [sg.Button("Exit")]
     ]
 
 # Set the initial window with the main layout
-window = sg.Window("Router Example", login_layout())
+window = sg.Window("Router Example", login())
 
 # Event loop for routing
 while True:
@@ -52,30 +52,33 @@ while True:
         # grab user and password
         username = values['-USERNAME-']
         password = values['-PASSWORD-']
-        window = sg.Window("Home", home_layout(), finalize=True)
+        window = sg.Window("Home", home(), finalize=True)
         # this changes the title of the window to the user's name
         # probably also how we will want to update user info through GET calls
         window['-USERNAME-DISPLAY-'].update(username + "'s Home")   
 
     elif event == "Profile":
         window.close()
-        window = sg.Window("Profile", profile_layout())
+        window = sg.Window("Profile", profile())
     elif event == "Back to Login":
         window.close()
-        window = sg.Window("Login", login_layout())
+        window = sg.Window("Login", login())
     elif event == "Home":
         window.close()
-        window = sg.Window("Home", home_layout(), finalize=True)
+        window = sg.Window("Home", home(), finalize=True)
         window['-USERNAME-DISPLAY-'].update(username + "'s Home")
     elif event == "Add Record":
         window.close()
-        window = sg.Window("Record", add_record_layout(), finalize=True)
+        window = sg.Window("Record", add_record(), finalize=True)
     elif event == "Submit":
         # this is where we push information to for new record to database
         print("Term: " +  str(values['-TERM-']))
         print("Definition: " +  str(values['-DEF-']))
 
+        window['-DEF-'].update("")
+        window['-TERM-'].update("")
         window['-ADDRECORD-DISPLAY-'].update("Term " + str(values['-TERM-']) + " added!")
+        
 
     # name = values[0]
     # password = values[1]
