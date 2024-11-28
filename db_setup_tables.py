3b4b0663ab02d748929687b23ec3f4384871f091
Tumi Oguntola <tumi.oguntola@gmail.com>
2024-11-28 05:48:24 -0500

    added user and post defaults & requirements

--- db_setup_tables.py
+++ db_setup_tables.py
@@ -11,8 +11,8 @@
 create_user_table = """CREATE TABLE users (
 uid int NOT NULL,
 name text NOT NULL,
-score int,
-password text,
+score int DEFAULT 0,
+password text NOT NULL,
 PRIMARY KEY (uid)
 )
 """
@@ -22,9 +22,9 @@ create_post_table = """CREATE TABLE posts (
 pid int NOT NULL,
 word text NOT NULL,
 definition text NOT NULL,
-upvotes int,
-downvotes int,
-uid int,
+upvotes int DEFAULT 0,
+downvotes int DEFAULT 0,
+uid int NOT NULL,
 PRIMARY KEY (pid),
 FOREIGN KEY (uid) REFERENCES users(uid)
 )"""
