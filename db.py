2819a1256c27a65ccab404b076fd0b3a96fca2a6
Tumi Oguntola <tumi.oguntola@gmail.com>
2024-11-28 05:49:01 -0500

    added docstrings and rudimentary login logic

--- db.py
+++ db.py
@@ -1,33 +1,66 @@
+"""This module contains functions for interacting with the database."""
 import mysql.connector
 import db_setup_tables
+import hashlib
 
+
+# DB connection info
 DB_HOST = "localhost"
 DB_USER = "root"
 DB_PASSWORD = "twentyone"
 DB_NAME = "mydatabase"
 
-mydb = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD)
+# DB connection and cursor
+mydb: mysql.connector.connection.MySQLConnection = None
+cursor: mysql.connector.cursor.MySQLCursor = None
 
-cursor = mydb.cursor()
-cursor.execute(f"USE {DB_NAME}")
+
+def init(): 
+    """Initializes the database connection and cursor."""
+    global mydb, cursor
+    mydb = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD)
+    cursor = mydb.cursor()
+    cursor.execute(f"USE {DB_NAME}")
 
 def get_cursor():
+    """Returns the cursor."""
     return cursor
   
 def close_cursor():
+    """Closes the cursor."""
     cursor.close()
   
 def create_user(name: str, score: int, password: str):
+    """Creates a user in the database."""
     cursor.execute(f"INSERT INTO users (name, score, password) VALUES ('{name}', {score}, '{password}')")
     
 def create_post(word: str, definition: str, upvotes: int, downvotes: int, uid: int):
+    """Creates a post in the database."""
     cursor.execute(f"INSERT INTO posts (word, definition, upvotes, downvotes, uid) VALUES ('{word}', '{definition}', {upvotes}, {downvotes}, {uid})")
     
 def create_interaction(uid: int, pid: int, action: bool):
+    """Creates an interaction in the database."""
     cursor.execute(f"INSERT INTO interactions (uid, pid, action) VALUES ({uid}, {pid}, {action})")
 
+def login_user(name: str, password: str):
+    """Logs in a user."""
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    print(f"Hashed password: {hashed_password}")
+    query = f"SELECT name FROM users WHERE name = '{name}' AND password = '{hashed_password}'"
+    cursor.execute(query)
+    output = cursor.fetchall()
+    if len(output) == 0:
+        return False
+    print(output)
+    if output[0][0] == name:
+        return True
+    return False
+
 if __name__ == "__main__":
-    init_cursor = mydb.cursor()
+    # Initializes the database and creates the tables.
+    init_db = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD)
+
+    init_cursor = init_db.cursor()
 
     init_cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
     init_cursor.execute(f"USE {DB_NAME}")
